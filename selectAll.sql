bf90329431d22ce428ec4bf67f58bb3e6fe97498
Eliaou-Madar <116590197+Eliaou-Madar@users.noreply.github.com>
2024-05-27 23:42:45 +0300

    שלב א

---
+++ selectAll.sql
@@ -0,0 +1,25 @@
+-- Create the selectAll.sql file with SELECT statements for all tables
+
+-- Select all data from the Patient table
+SELECT * FROM Patient;
+
+-- Select all data from the Operating_Room table
+SELECT * FROM Operating_Room;
+
+-- Select all data from the Equipement table
+SELECT * FROM Equipement;
+
+-- Select all data from the Doctor table
+SELECT * FROM Doctor;
+
+-- Select all data from the Nurse table
+SELECT * FROM Nurse;
+
+-- Select all data from the Operation table
+SELECT * FROM Operation;
+
+-- Select all data from the Operate_by table
+SELECT * FROM Operate_by;
+
+-- Select all data from the Assist_by table
+SELECT * FROM Assist_by;
